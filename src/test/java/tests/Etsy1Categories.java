4520f915d69695da94e8ecb9d1e3f2047942d38c
Anna <annapacovskaa@MacBook-Air-Anna.local>
2024-03-18 22:55:54 -0400

    Anna's tests

---
+++ src/test/java/tests/Etsy1Categories.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+
+public class Etsy1Categories {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("WebDriver.chrome.driver", "/Users/annapacovskaa/.cache/selenium/chromedriver/mac-arm64/122.0.6261.128");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://www.etsy.com/");
+        Thread.sleep(5000);
+
+        WebElement categoriesLink = driver.findElement(By.xpath("//*[@id=\"gnav-header-inner\"]/nav/div/button"));
+        categoriesLink.click();
+    }
+}
---
+++ src/test/java/tests/Etsy6ShowMore.java
@@ -0,0 +1,24 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+
+public class Etsy6ShowMore {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("WebDriver.chrome.driver", "/Users/annapacovskaa/.cache/selenium/chromedriver/mac-arm64/122.0.6261.128");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.etsy.com/c/bags-and-purses?ref=catnav-132");
+        Thread.sleep(2000);
+
+        driver.findElement(By.xpath("//*[@id=\"additional-categories__trigger--more\"]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//button[@title='Show more (11)']")).click();
+
+    }
+}
