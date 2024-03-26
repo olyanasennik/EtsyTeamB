b06eae87d1e01295d3c697508990025d5e2297da
olyanasennik <nasennikolya@gmail.com>
2024-03-25 21:02:41 -0400

    update3

--- src/test/java/tests/OpenEtsyShop.java
+++ src/test/java/tests/OpenEtsyShop.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import pages.SellerPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.SeleniumUtils;
 
 import java.util.Random;
 import java.util.Set;
@@ -26,59 +27,66 @@ public class OpenEtsyShop extends TestBase {
         //Select button Create your Shop
         Driver.getDriver().findElement(By.xpath("//a[@href='/your/shop/create?ref-seller-handbook=seller-handbook-open-shop-logged-in&ref=handbook']")).click();
         //Shop preferences page - language dropdown
-        WebElement dropdownLang = driver.findElement(By.id("onboard-shop-language"));
+        WebElement dropdownLang = Driver.getDriver().findElement(By.id("onboard-shop-language"));
         Select selectLang = new Select(dropdownLang);
         selectLang.selectByValue("es");
         // - country dropdown
-        WebElement dropdownCountry = driver.findElement(By.id("address-country"));
+        WebElement dropdownCountry = Driver.getDriver().findElement(By.id("address-country"));
         Select selectCountry = new Select(dropdownCountry);
         selectCountry.selectByValue("99");
 
+        //SeleniumUtils.jsClick(dropdownCountry);
         // - currency dropdown
-        WebElement dropdownCurrency = driver.findElement(By.id("onboard-shop-currency"));
+        WebElement dropdownCurrency = Driver.getDriver().findElement(By.id("onboard-shop-currency"));
         Select selectCurrency = new Select(dropdownCurrency);
         selectCurrency.selectByValue("EUR");
 
+        //SeleniumUtils.jsClick(dropdownCurrency);
+
         // Button Save&Continue using JS Executor
-        WebElement saveContinue = driver.findElement(By.linkText("Save and continue"));
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        WebElement saveContinue = Driver.getDriver().findElement(By.linkText("Save and continue"));
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) Driver.getDriver();
         jsExecutor.executeScript("arguments[0].click();", saveContinue);
+ SeleniumUtils.jsClick(saveContinue);
 
         //Enter your shop name - create random name
-            Driver.getDriver().findElement(By.id("onboarding-shop-name-input")).sendKeys(generateRandomPassword());
+        Driver.getDriver().findElement(By.id("onboarding-shop-name-input")).sendKeys(generateRandomPassword());
 
-        }
+        SeleniumUtils.jsClick(saveContinue);
 //Select button Save&Continue
-        WebElement saveContinue1 = driver.findElement(By.linkText("Save and continue"));
-        JavascriptExecutor jsExecutor1 = (JavascriptExecutor)  driver;
+        WebElement saveContinue1 = Driver.getDriver().findElement(By.linkText("Save and continue"));
+        JavascriptExecutor jsExecutor1 = (JavascriptExecutor) Driver.getDriver();
         jsExecutor1.executeScript("arguments[0].click();", saveContinue1);
+        SeleniumUtils.jsClick(saveContinue1);
 
         //catch Handle for the 1st window
-       String firstWindowHandle = getDriver().getWindowHandle();
+        String firstWindowHandle = getDriver().getWindowHandle();
 
         //Open "Learn more what can be seen on Etsy link" that opens 2 nd window
-        WebElement learnMore = driver.findElement(By.xpath("//a[@href='https://help.etsy.com/hc/en-us/articles/360024112614-What-Can-I-Sell-on-Etsy-?segment=selling']"));
-        JavascriptExecutor jsLearnMore = (JavascriptExecutor) driver;
+        WebElement learnMore = Driver.getDriver().findElement(By.xpath("//a[@href='https://help.etsy.com/hc/en-us/articles/360024112614-What-Can-I-Sell-on-Etsy-?segment=selling']"));
+        JavascriptExecutor jsLearnMore = (JavascriptExecutor) Driver.getDriver();
         jsLearnMore.executeScript("arguments[0].click();", learnMore);
 
+        SeleniumUtils.jsClick(learnMore);
         //catch all Window Handles
-        Set<String> windowHandles = driver.getWindowHandles();
+        Set<String> windowHandles = Driver.getDriver().getWindowHandles();
 
         // Switch back to the first window
-        for (String handle : windowHandles){
+        for (String handle : windowHandles) {
             if (handle.equals(firstWindowHandle)) {
-                driver.switchTo().window(handle);
+                Driver.getDriver().switchTo().window(handle);
                 break;
             }
         }
 
         //Select button Save&Continue
-        WebElement saveContinue2 = driver.findElement(By.linkText("Save and continue"));
-        JavascriptExecutor jsExecutor2 = (JavascriptExecutor)  driver;
+        WebElement saveContinue2 = Driver.getDriver().findElement(By.linkText("Save and continue"));
+        JavascriptExecutor jsExecutor2 = (JavascriptExecutor) driver;
         jsExecutor2.executeScript("arguments[0].click();", saveContinue2);
 
+        SeleniumUtils.jsClick(saveContinue2);
 
-
+    }
     }
 
 
